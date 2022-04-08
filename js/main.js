d3000767133d33ec15f07d78196128f708beb79a
Mostafa Mohammad Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-04-08 14:24:02 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,26 @@ $(function () {
 
   $('#wrapper').show();
   $('#loader').slideUp().remove();
+
+    var KEY_CODE,
+      isHorizontal,
+      // AvroParser instance
+      avro,
+      // Array of drafs
+      drafts = [],
+      // Candidate Window Selected Item Index
+      selectedIndex,
+      toggleLanguage,
+      fetchAllDrafts,
+      loadDraftId,
+      currentDraftId,
+      makeTitle,
+      $editor = $('textarea'),
+      $statusControl = $('#state'),
+      $current,
+      isBN = true,
+      // Length of draft title in chars
+      titleLength = 25,
+      LS = window.localStorage,
+      runningEvent = 0,
+      selectedTpl = '<li class="cur" data-value="${name}"><a href="#">${name}</a></li>';
