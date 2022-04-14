3051e0a8aa61fbd8d4f1f61a6953ad1f5cd785f8
Mostafa Mohammad Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-04-14 10:33:24 +0600

    Update misc.js

--- js/misc.js
+++ js/misc.js
@@ -1,1 +1,8 @@
+document.getElementById("myCheckbox").addEventListener("change", switchMode);
+function applyDark(){
+    $("#wrapper").addClass("wrapper_dark");
+    $("#wrapper").removeClass("wrapper_light");
 
+    $("textarea").addClass("text_dark_mode");
+    $("footer").addClass("footer_dark");
+}
