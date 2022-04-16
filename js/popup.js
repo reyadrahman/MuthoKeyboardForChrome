5f07e9c2fdab58766e954f2647b499dfd047512b
Mostafa Mohammad Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-04-16 11:14:50 +0600

    Update popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,2 +1,13 @@
 var saveButton = document.getElementById('saveButton');
 var selection = document.getElementById('selection');
+
+chrome.storage.local.get('banglaFont', function (items) {
+    var matched = false;
+    var opts = selection.options;
+    for (var opt, j = 0; opt = opts[j]; j++) {
+        if (opt.value == items.banglaFont) {
+            selection.selectedIndex = j;
+            matched = true;
+            break;
+        }
+    }
