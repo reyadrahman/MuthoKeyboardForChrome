730ce3d70e26461d61e7562cf0c031ed4f2ef2cf
Mostafa Mohammad Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-04-17 14:18:20 +0600

    Update jquery.caret.js

--- vendor/Caret.js/src/jquery.caret.js
+++ vendor/Caret.js/src/jquery.caret.js
@@ -1,1 +1,8 @@
+//@ sourceMappingURL=jquery.caret.map
+/*
+  Implement Github like autocomplete mentions
+  http://ichord.github.com/At.js
 
+  Copyright (c) 2013 chord.luo@gmail.com
+  Licensed under the MIT license.
+*/
