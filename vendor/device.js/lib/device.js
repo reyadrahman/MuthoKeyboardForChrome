184d419b0f567f271659d522f0f70cc06a920cf3
Mostafa Mohammad Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-04-26 16:29:52 +0600

    Update device.js

--- vendor/device.js/lib/device.js
+++ vendor/device.js/lib/device.js
@@ -1,1 +1,5 @@
-
+// Device.js
+// (c) 2014 Matthew Hudson
+// Device.js is freely distributable under the MIT license.
+// For all details and documentation:
+// http://matthewhudson.me/projects/device.js/
