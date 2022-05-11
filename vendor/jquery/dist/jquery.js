8117105a783a5350a1ee0afed6a4466b57f5f7c1
Mostafa Mohammad Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-05-11 11:39:40 +0600

    Update jquery.js

--- vendor/jquery/dist/jquery.js
+++ vendor/jquery/dist/jquery.js
@@ -105,3 +105,9 @@ jQuery.fn = jQuery.prototype = {
 	// Get the whole matched element set as a clean array
 	get: function( num ) {
 		return num != null ?
+		// Return just the one element from the set
+			( num < 0 ? this[ num + this.length ] : this[ num ] ) :
+
+			// Return all the elements in a clean array
+			slice.call( this );
+	},
