a53a2c92892e19871fb74c1bad32f1e8bdf0b6d9
Mostafa Mohammad Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-05-17 11:46:48 +0600

    Update jquery.js

--- vendor/jquery/dist/jquery.js
+++ vendor/jquery/dist/jquery.js
@@ -398,4 +398,14 @@ jQuery.extend = jQuery.fn.extend = function() {
 				// Return the modified object
 	          return target;
              };
-				
+		jQuery.extend({
+	// Unique for each copy of jQuery on the page
+	expando: "jQuery" + ( version + Math.random() ).replace( /\D/g, "" ),
+
+	// Assume jQuery is ready without the ready module
+	isReady: true,
+
+	error: function( msg ) {
+		throw new Error( msg );
+	},
+		
