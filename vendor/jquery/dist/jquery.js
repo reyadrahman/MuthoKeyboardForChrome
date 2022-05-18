f71e6deed7e94cd5247e150a70b896f790d870d1
Mostafa Mohammad Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-05-18 10:52:00 +0600

    Update jquery.js

--- vendor/jquery/dist/jquery.js
+++ vendor/jquery/dist/jquery.js
@@ -362,3 +362,27 @@ jQuery.extend({
 					}
 				}
 			}
+
+	// A special, fast, case for the most common use of each
+		} else {
+			if ( isArray ) {
+				for ( ; i < length; i++ ) {
+					value = callback.call( obj[ i ], i, obj[ i ] );
+
+					if ( value === false ) {
+						break;
+					}
+				}
+			} else {
+				for ( i in obj ) {
+					value = callback.call( obj[ i ], i, obj[ i ] );
+
+					if ( value === false ) {
+						break;
+					}
+				}
+			}
+		}
+
+		return obj;
+	},
