1edec84a6bcc2c28b31ce2e50f98c2a26b6431c0
Mostafa Mohammad Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-05-29 01:19:06 +0600

    Update jquery.js

--- vendor/jquery/dist/jquery.js
+++ vendor/jquery/dist/jquery.js
@@ -1125,3 +1125,23 @@ setDocument = Sizzle.setDocument = function( node ) {
 				return context.querySelectorAll( tag );
 			}
 		} :
+
+	function( tag, context ) {
+			var elem,
+				tmp = [],
+				i = 0,
+				// By happy coincidence, a (broken) gEBTN appears on DocumentFragment nodes too
+				results = context.getElementsByTagName( tag );
+
+			// Filter out possible comments
+			if ( tag === "*" ) {
+				while ( (elem = results[i++]) ) {
+					if ( elem.nodeType === 1 ) {
+						tmp.push( elem );
+					}
+				}
+
+				return tmp;
+			}
+			return results;
+		};
