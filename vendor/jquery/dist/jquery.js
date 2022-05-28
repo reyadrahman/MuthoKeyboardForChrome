65b8c632ceebc2737c6274aca77c0a15e20cd85c
Mostafa Mohammad Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-05-28 06:40:40 +0600

    Update jquery.js

--- vendor/jquery/dist/jquery.js
+++ vendor/jquery/dist/jquery.js
@@ -990,3 +990,27 @@ function createButtonPseudo( type ) {
 		return (name === "input" || name === "button") && elem.type === type;
 	};
 }
+
+	
+	
+/**
+ * Returns a function to use in pseudos for positionals
+ * @param {Function} fn
+ */
+function createPositionalPseudo( fn ) {
+	return markFunction(function( argument ) {
+		argument = +argument;
+		return markFunction(function( seed, matches ) {
+			var j,
+				matchIndexes = fn( [], seed.length, argument ),
+				i = matchIndexes.length;
+
+			// Match elements found at the specified indexes
+			while ( i-- ) {
+				if ( seed[ (j = matchIndexes[i]) ] ) {
+					seed[j] = !(matches[j] = seed[j]);
+				}
+			}
+		});
+	});
+}
