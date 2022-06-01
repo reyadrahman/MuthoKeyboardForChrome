7289549320fd521c1b9759aca44bc33fc07466a4
Mostafa Mohammad Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-06-01 06:02:07 +0600

    Update jquery.js

--- vendor/jquery/dist/jquery.js
+++ vendor/jquery/dist/jquery.js
@@ -1644,3 +1644,13 @@ Expr = Sizzle.selectors = {
 					return elem.nodeName && elem.nodeName.toLowerCase() === nodeName;
 				};
 		},
+
+		"CLASS": function( className ) {
+			var pattern = classCache[ className + " " ];
+
+			return pattern ||
+				(pattern = new RegExp( "(^|" + whitespace + ")" + className + "(" + whitespace + "|$)" )) &&
+				classCache( className, function( elem ) {
+					return pattern.test( typeof elem.className === "string" && elem.className || typeof elem.getAttribute !== "undefined" && elem.getAttribute("class") || "" );
+				});
+		},
