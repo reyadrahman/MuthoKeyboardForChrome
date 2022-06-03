c9537bc787949d8f200c9558a48bbccb44c1fc96
Mostafa Mohammad Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-06-03 07:46:24 +0600

    Update jquery.js

--- vendor/jquery/dist/jquery.js
+++ vendor/jquery/dist/jquery.js
@@ -2139,3 +2139,18 @@ function toSelector( tokens ) {
 			}
 		};
 }
+
+	
+function elementMatcher( matchers ) {
+	return matchers.length > 1 ?
+		function( elem, context, xml ) {
+			var i = matchers.length;
+			while ( i-- ) {
+				if ( !matchers[i]( elem, context, xml ) ) {
+					return false;
+				}
+			}
+			return true;
+		} :
+		matchers[0];
+}
