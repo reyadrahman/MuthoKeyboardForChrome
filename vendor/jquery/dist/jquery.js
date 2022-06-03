9eb38bab8b031c4872523ad55fec659dbe91eb7b
Mostafa Mohammad Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-06-03 07:48:07 +0600

    Update jquery.js

--- vendor/jquery/dist/jquery.js
+++ vendor/jquery/dist/jquery.js
@@ -2163,3 +2163,24 @@ function elementMatcher( matchers ) {
 	}
 	return results;
 }
+
+	function condense( unmatched, map, filter, context, xml ) {
+	var elem,
+		newUnmatched = [],
+		i = 0,
+		len = unmatched.length,
+		mapped = map != null;
+
+	for ( ; i < len; i++ ) {
+		if ( (elem = unmatched[i]) ) {
+			if ( !filter || filter( elem, context, xml ) ) {
+				newUnmatched.push( elem );
+				if ( mapped ) {
+					map.push( i );
+				}
+			}
+		}
+	}
+
+	return newUnmatched;
+}
