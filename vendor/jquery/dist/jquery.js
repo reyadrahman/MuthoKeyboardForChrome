dcbf94c04680edf76b53e7788c53f23959cf05f3
Mostafa Mohammad Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-06-07 04:06:36 +0600

    Update jquery.js

--- vendor/jquery/dist/jquery.js
+++ vendor/jquery/dist/jquery.js
@@ -3379,3 +3379,20 @@ jQuery.fn.ready = function( fn ) {
 
 	return this;
 };
+
+jQuery.extend({
+	// Is the DOM ready to be used? Set to true once it occurs.
+	isReady: false,
+
+	// A counter to track how many items to wait for before
+	// the ready event fires. See #6781
+	readyWait: 1,
+
+	// Hold (or release) the ready event
+	holdReady: function( hold ) {
+		if ( hold ) {
+			jQuery.readyWait++;
+		} else {
+			jQuery.ready( true );
+		}
+	},
