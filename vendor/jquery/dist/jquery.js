c511b72c6ce75bf1d7b8e4e7803fd8112321f83e
Mostafa Mohammad Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-06-10 14:29:33 +0600

    Update jquery.js

--- vendor/jquery/dist/jquery.js
+++ vendor/jquery/dist/jquery.js
@@ -3827,3 +3827,24 @@ jQuery.fn.extend({
 				return;
 			}
 
+
+
+jQuery.extend({
+	queue: function( elem, type, data ) {
+		var queue;
+
+		if ( elem ) {
+			type = ( type || "fx" ) + "queue";
+			queue = data_priv.get( elem, type );
+
+			// Speed up dequeue by getting out quickly if this is just a lookup
+			if ( data ) {
+				if ( !queue || jQuery.isArray( data ) ) {
+					queue = data_priv.access( elem, type, jQuery.makeArray(data) );
+				} else {
+					queue.push( data );
+				}
+			}
+			return queue || [];
+		}
+	},
