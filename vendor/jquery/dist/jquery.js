2eb9859de798fa1f9df2164745b08ad535fb3a31
Mostafa Mohammad Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-06-17 12:35:31 +0600

    Update jquery.js

--- vendor/jquery/dist/jquery.js
+++ vendor/jquery/dist/jquery.js
@@ -5031,3 +5031,17 @@ function getAll( context, tag ) {
 		jQuery.merge( [ context ], ret ) :
 		ret;
 	
+
+// Fix IE bugs, see support tests
+function fixInput( src, dest ) {
+	var nodeName = dest.nodeName.toLowerCase();
+
+	// Fails to persist the checked state of a cloned checkbox or radio button.
+	if ( nodeName === "input" && rcheckableType.test( src.type ) ) {
+		dest.checked = src.checked;
+
+	// Fails to return the selected option to the default selected state when cloning options
+	} else if ( nodeName === "input" || nodeName === "textarea" ) {
+		dest.defaultValue = src.defaultValue;
+	}
+}
