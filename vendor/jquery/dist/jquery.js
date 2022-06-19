3b802f169f989eb8a0b7d77169e6d2ee4cb0bd0e
Mostafa Mohammad Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-06-19 12:02:06 +0600

    Update jquery.js

--- vendor/jquery/dist/jquery.js
+++ vendor/jquery/dist/jquery.js
@@ -5278,3 +5278,21 @@ cleanData: function( elems ) {
 
 		return this;
 	},
+
+		empty: function() {
+		var elem,
+			i = 0;
+
+		for ( ; (elem = this[i]) != null; i++ ) {
+			if ( elem.nodeType === 1 ) {
+
+				// Prevent memory leaks
+				jQuery.cleanData( getAll( elem, false ) );
+
+				// Remove any remaining nodes
+				elem.textContent = "";
+			}
+		}
+
+		return this;
+	},
