4b360b735577b8a9ea46f4f36a3185be54e1885a
Mostafa Mohammad Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-06-24 07:17:49 +0600

    Update jquery.js

--- vendor/jquery/dist/jquery.js
+++ vendor/jquery/dist/jquery.js
@@ -6048,3 +6048,19 @@ jQuery.each([ "height", "width" ], function( i, name ) {
 					getWidthOrHeight( elem, name, extra );
 			}
 		},
+
+
+		set: function( elem, value, extra ) {
+			var styles = extra && getStyles( elem );
+			return setPositiveNumber( elem, value, extra ?
+				augmentWidthOrHeight(
+					elem,
+					name,
+					extra,
+					jQuery.css( elem, "boxSizing", false, styles ) === "border-box",
+					styles
+				) : 0
+			);
+		}
+	};
+});
