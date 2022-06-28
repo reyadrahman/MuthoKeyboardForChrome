70d29b3e4fbc37ab9f41e0f1ce7237c523041b2c
Mostafa Mohammad Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-06-28 12:58:08 +0600

    Update jquery.js

--- vendor/jquery/dist/jquery.js
+++ vendor/jquery/dist/jquery.js
@@ -6820,3 +6820,12 @@ jQuery.fn.extend({
 		});
 	}
 });
+
+	jQuery.each([ "toggle", "show", "hide" ], function( i, name ) {
+	var cssFn = jQuery.fn[ name ];
+	jQuery.fn[ name ] = function( speed, easing, callback ) {
+		return speed == null || typeof speed === "boolean" ?
+			cssFn.apply( this, arguments ) :
+			this.animate( genFx( name, true ), speed, easing, callback );
+	};
+});
