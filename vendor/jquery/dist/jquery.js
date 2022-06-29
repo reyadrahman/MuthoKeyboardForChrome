9325090e56aed638b81a294d17ff7536bd0fe1ee
Mostafa Mohammad Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-06-29 12:46:06 +0600

    Update jquery.js

--- vendor/jquery/dist/jquery.js
+++ vendor/jquery/dist/jquery.js
@@ -6843,3 +6843,26 @@ jQuery.each({
 		return this.animate( props, speed, easing, callback );
 	};
 });
+	
+	
+jQuery.timers = [];
+jQuery.fx.tick = function() {
+	var timer,
+		i = 0,
+		timers = jQuery.timers;
+
+	fxNow = jQuery.now();
+
+	for ( ; i < timers.length; i++ ) {
+		timer = timers[ i ];
+		// Checks the timer has not already been removed
+		if ( !timer() && timers[ i ] === timer ) {
+			timers.splice( i--, 1 );
+		}
+	}
+
+	if ( !timers.length ) {
+		jQuery.fx.stop();
+	}
+	fxNow = undefined;
+};
