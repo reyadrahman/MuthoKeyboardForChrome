bd62ae8597bd237cf2fd8daf3c40e0fcee0293ad
Mostafa Mohammad Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-07-01 15:59:48 +0600

    Update jquery.js

--- vendor/jquery/dist/jquery.js
+++ vendor/jquery/dist/jquery.js
@@ -7006,3 +7006,24 @@ jQuery.fn.extend({
 				ret;
 		}
 	},
+
+		
+		removeAttr: function( elem, value ) {
+		var name, propName,
+			i = 0,
+			attrNames = value && value.match( rnotwhite );
+
+		if ( attrNames && elem.nodeType === 1 ) {
+			while ( (name = attrNames[i++]) ) {
+				propName = jQuery.propFix[ name ] || name;
+
+				// Boolean attributes get special treatment (#10870)
+				if ( jQuery.expr.match.bool.test( name ) ) {
+					// Set corresponding property to false
+					elem[ propName ] = false;
+				}
+
+				elem.removeAttribute( name );
+			}
+		}
+	},
