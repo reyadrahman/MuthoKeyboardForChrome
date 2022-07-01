2787a0e66b18e490698b88dfc5e54daa9814754d
Mostafa Mohammad Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-07-01 16:10:46 +0600

    Update jquery.js

--- vendor/jquery/dist/jquery.js
+++ vendor/jquery/dist/jquery.js
@@ -7075,3 +7075,18 @@ boolHook = {
 		return ret;
 	};
 });
+
+	
+	var rfocusable = /^(?:input|select|textarea|button)$/i;
+
+jQuery.fn.extend({
+	prop: function( name, value ) {
+		return access( this, jQuery.prop, name, value, arguments.length > 1 );
+	},
+
+	removeProp: function( name ) {
+		return this.each(function() {
+			delete this[ jQuery.propFix[ name ] || name ];
+		});
+	}
+});
