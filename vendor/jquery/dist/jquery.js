c8d67c1ef9bf96891c3b0522499bff3e8c861b2f
Mostafa Mohammad Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-07-03 14:22:17 +0600

    Update jquery.js

--- vendor/jquery/dist/jquery.js
+++ vendor/jquery/dist/jquery.js
@@ -7617,3 +7617,29 @@ function addToPrefiltersOrTransports( structure ) {
 		}
 	};
 }
+
+	
+	// Base inspection function for prefilters and transports
+function inspectPrefiltersOrTransports( structure, options, originalOptions, jqXHR ) {
+
+	var inspected = {},
+		seekingTransport = ( structure === transports );
+
+	function inspect( dataType ) {
+		var selected;
+		inspected[ dataType ] = true;
+		jQuery.each( structure[ dataType ] || [], function( _, prefilterOrFactory ) {
+			var dataTypeOrTransport = prefilterOrFactory( options, originalOptions, jqXHR );
+			if ( typeof dataTypeOrTransport === "string" && !seekingTransport && !inspected[ dataTypeOrTransport ] ) {
+				options.dataTypes.unshift( dataTypeOrTransport );
+				inspect( dataTypeOrTransport );
+				return false;
+			} else if ( seekingTransport ) {
+				return !( selected = dataTypeOrTransport );
+			}
+		});
+		return selected;
+	}
+
+	return inspect( options.dataTypes[ 0 ] ) || !inspected[ "*" ] && inspect( "*" );
+}
