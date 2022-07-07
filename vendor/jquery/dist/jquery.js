af9a18770fe6a0b2e1f8f53c79d0130ceccd4318
Mostafa Mohammad Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-07-07 23:47:11 +0600

    Update jquery.js

--- vendor/jquery/dist/jquery.js
+++ vendor/jquery/dist/jquery.js
@@ -8483,3 +8483,12 @@ jQuery.extend({
 		}).end();
 	}
 });
+
+jQuery.expr.filters.hidden = function( elem ) {
+	// Support: Opera <= 12.12
+	// Opera reports offsetWidths and offsetHeights less than zero on some elements
+	return elem.offsetWidth <= 0 && elem.offsetHeight <= 0;
+};
+jQuery.expr.filters.visible = function( elem ) {
+	return !jQuery.expr.filters.hidden( elem );
+};
