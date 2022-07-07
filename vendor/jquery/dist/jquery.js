75fc21e38e6150d385ab958d27d4e9ce442ee58f
Mostafa Mohammad Reyad Rahman <reyadrahman@users.noreply.github.com>
2022-07-07 23:56:29 +0600

    Update jquery.js

--- vendor/jquery/dist/jquery.js
+++ vendor/jquery/dist/jquery.js
@@ -8564,3 +8564,36 @@ jQuery.param = function( a, traditional ) {
 	// Return the resulting serialization
 	return s.join( "&" ).replace( r20, "+" );
 };
+
+	
+	jQuery.fn.extend({
+	serialize: function() {
+		return jQuery.param( this.serializeArray() );
+	},
+	serializeArray: function() {
+		return this.map(function() {
+			// Can add propHook for "elements" to filter or add form elements
+			var elements = jQuery.prop( this, "elements" );
+			return elements ? jQuery.makeArray( elements ) : this;
+		})
+		.filter(function() {
+			var type = this.type;
+
+			// Use .is( ":disabled" ) so that fieldset[disabled] works
+			return this.name && !jQuery( this ).is( ":disabled" ) &&
+				rsubmittable.test( this.nodeName ) && !rsubmitterTypes.test( type ) &&
+				( this.checked || !rcheckableType.test( type ) );
+		})
+		.map(function( i, elem ) {
+			var val = jQuery( this ).val();
+
+			return val == null ?
+				null :
+				jQuery.isArray( val ) ?
+					jQuery.map( val, function( val ) {
+						return { name: elem.name, value: val.replace( rCRLF, "\r\n" ) };
+					}) :
+					{ name: elem.name, value: val.replace( rCRLF, "\r\n" ) };
+		}).get();
+	}
+});
